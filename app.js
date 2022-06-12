b36e7f41696c12edf54f7aa3310999087ba09386
Maor <Maor@MAOR-PC>
2022-06-12 21:15:31 +0300

    Add HTTPS protocol

--- app.js
+++ app.js
@@ -1,6 +1,12 @@
 const app = require('./server')
+const https = require('https')
+const http = require('http')
 
-const port = process.env.PORT
-app.listen(port, ()=>{
-    console.log('server is running on port ' + port)
-})
+const httpPort = process.env.HTTP_PORT
+const httpsPort = process.env.HTTPS_PORT
+
+http.createServer(app).listen(httpPort,()=>{
+    console.log('HTTP- server is running on port ' + httpPort)});
+
+https.createServer(app).listen(httpsPort,()=>{
+    console.log('HTTPS- server is running on port ' + httpsPort)});
