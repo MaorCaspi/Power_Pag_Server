f4bd0a175b77643004e933c4e7ad01b0ee89bc2b
Maor <Maor@MAOR-PC>
2022-06-12 23:23:06 +0300

    Remove https

--- app.js
+++ app.js
@@ -1,12 +1,7 @@
 const app = require('./server')
-const https = require('https')
-const http = require('http')
 
-const httpPort = process.env.HTTP_PORT
-const httpsPort = process.env.HTTPS_PORT
+const port = process.env.port
 
-http.createServer(app).listen(httpPort,()=>{
-    console.log('HTTP- server is running on port ' + httpPort)});
-
-https.createServer(app).listen(httpsPort,()=>{
-    console.log('HTTPS- server is running on port ' + httpsPort)});
+app.listen(port, ()=>{
+    console.log('server is running on port ' + port)
+}) 
